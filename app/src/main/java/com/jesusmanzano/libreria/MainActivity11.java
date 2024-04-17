0f53c4a3f4f5a6d006659dc97b6017fee49cdf13
JesusManzano24 <9991404825j@gmail.com>
2024-04-17 07:10:09 -0600

    onceavo Commit Figura1 Ninoi Funciones E Interfaz

---
+++ app/src/main/java/com/jesusmanzano/libreria/MainActivity11.java
@@ -0,0 +1,79 @@
+package com.jesusmanzano.panaderia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity11 extends AppCompatActivity {
+    ImageButton Atras;
+    Button pagar;
+    Button agregar;
+    Button quitar;
+    TextView cantidad;
+    TextView total;
+    int cantidadFiguras = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main11);
+
+
+        pagar = (Button) findViewById(R.id.pagar);
+        agregar = (Button) findViewById(R.id.agregar);
+        quitar = (Button) findViewById(R.id.Quitar);
+        cantidad = (TextView) findViewById(R.id.cantidad);
+        total = (TextView) findViewById(R.id.total);
+        Atras = findViewById(R.id.atras);
+
+        Atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentSora = new Intent(MainActivity11.this, MainActivity8.class);
+                startActivity(intentSora);
+            }
+        });
+        pagar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentPagar = new Intent(MainActivity11.this, MainActivity4.class);//falta el mainde pago
+                startActivity(intentPagar);
+            }
+        });
+        agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cantidadFiguras++;
+                cantidad.setText("Cantidad: " + cantidadFiguras);
+                actualizarTotal();
+            }
+        });
+        quitar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cantidadFiguras > 0) {
+                    cantidadFiguras--;
+                    cantidad.setText("Cantidad: " + cantidadFiguras);
+                    actualizarTotal();
+                }
+            }
+        });
+    }
+
+    private void actualizarTotal() {
+        int TotalpagarFiguraNino = cantidadFiguras * 2572;
+        total.setText("Total: " + TotalpagarFiguraNino);
+
+        // Guardar el total en SharedPreferences
+        SharedPreferences prefs = getSharedPreferences("PreferenciasDePago", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("TotalpagarFiguraNino", TotalpagarFiguraNino);
+        editor.apply(); // Guardar los cambios
+    }
+}
