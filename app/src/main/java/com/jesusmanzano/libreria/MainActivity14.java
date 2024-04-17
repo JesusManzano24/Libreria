ff59e5e5c61feca5ab4826e3e9cd8cf8b953abe1
JesusManzano24 <9991404825j@gmail.com>
2024-04-17 07:13:57 -0600

    Catorceavo Commit Mangas We never learn Funciones E Interfaz

---
+++ app/src/main/java/com/jesusmanzano/libreria/MainActivity14.java
@@ -0,0 +1,77 @@
+package com.jesusmanzano.panaderia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity14 extends AppCompatActivity {
+    ImageButton Atras;
+    Button pagar;
+    Button agregar;
+    Button quitar;
+    TextView cantidad;
+    TextView total;
+    int cantidadBoxset = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main14);
+
+
+        pagar = findViewById(R.id.pagar);
+        agregar =  findViewById(R.id.agregar);
+        quitar =  findViewById(R.id.Quitar);
+        cantidad =  findViewById(R.id.cantidad);
+        total =  findViewById(R.id.total);
+        Atras = findViewById(R.id.atras);
+
+        Atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentSora = new Intent(MainActivity14.this, MainActivity12.class);
+                startActivity(intentSora);
+            }
+        });
+        pagar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentPagar = new Intent(MainActivity14.this, MainActivity4.class);//falta el mainde pago
+                startActivity(intentPagar);
+            }
+        });
+        agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cantidadBoxset++;
+                cantidad.setText("Cantidad: " + cantidadBoxset);
+                actualizarTotal();
+            }
+        });
+        quitar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cantidadBoxset > 0) {
+                    cantidadBoxset--;
+                    cantidad.setText("Cantidad: " + cantidadBoxset);
+                    actualizarTotal();
+                }
+            }
+        });
+    }
+    private void actualizarTotal() {
+        int TotalpagarMangawenever = cantidadBoxset * 129;
+        total.setText("Total: " + TotalpagarMangawenever);
+
+        SharedPreferences prefs = getSharedPreferences("PreferenciasDePago", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("TotalpagarMangawenever", TotalpagarMangawenever);
+        editor.apply(); // Guardar los cambios
+    }
+}
